0ca9b15c8e53bc3e56ee808e278c411f9fcfe952
Alastair <ala53@users.noreply.github.com>
2015-07-06 14:03:40 -0700

    Include Nuget package restore

--- prebuild.cmd
+++ prebuild.cmd
@@ -1,5 +1,10 @@
 SET msbuildpath="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
+nuget restore %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln
+nuget restore %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln
+nuget restore %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln
+nuget restore %~dp0\MPTanks-MK5\MPTanks-MK5.sln
+
 %msbuildpath% %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Release
 %msbuildpath% %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Release
 %msbuildpath% %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Release
