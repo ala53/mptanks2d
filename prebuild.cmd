e6f57823a7c38bb069558ac0db6013cdda433cd1
Alastair <ala53@users.noreply.github.com>
2015-07-05 17:06:07 -0700

    Added build event to call prebuild.cmd
    If prebuild.cmd was not called before the solution was loaded, it will
    be called on build.

--- prebuild.cmd
+++ prebuild.cmd
@@ -1,11 +1,9 @@
 SET msbuildpath="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
-%msbuildpath% Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Release
-%msbuildpath% Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Release
-%msbuildpath% Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Release
+%msbuildpath% %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Release
+%msbuildpath% %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Release
+%msbuildpath% %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Release
 
-%msbuildpath% Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Debug
-%msbuildpath% Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Debug
-%msbuildpath% Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Debug
-
-pause
+%msbuildpath% %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Debug
+%msbuildpath% %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Debug
+%msbuildpath% %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Debug
