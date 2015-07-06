cf149911a6912458fbafc3a5841db1cf4a257031
Alastair <ala53@users.noreply.github.com>
2015-07-06 14:57:00 -0700

    Recreate core-assets.mod
    prebuild.cmd creates a core-assets.mod file, to prevent build errors.

--- prebuild.cmd
+++ prebuild.cmd
@@ -1,5 +1,7 @@
 SET msbuildpath="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
+echo "blank" > %~dp0\MPTanks-MK5\CoreAssets\core-assets.mod
+
 nuget restore %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln
 nuget restore %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln
 nuget restore %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln
