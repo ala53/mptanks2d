b8fc8d5f55361f5633337788f55b57583b46c183
Alastair <c4dydl1@gmail.com>
2015-07-19 01:10:20 -0700

    Create generic wall object
    Now there's a simple 1x1 block wall object

---
+++ MPTanks-MK5/CoreAssets/MapObjects/Wall.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using MPTanks.Engine;
+using MPTanks.Engine.Maps.MapObjects;
+using MPTanks.Modding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPTanks.CoreAssets.MapObjects
+{
+    [MapObject("WallGeneric", "wall_generic.json", IsStatic = true, DisplayName = "Generic Wall",
+        Description = "A generic, bland wall type.")]
+    public class Wall : MapObject
+    {
+        public Wall(GameCore game, bool authorized,
+            Vector2 position = default(Vector2), float rotation = 0) :
+                base(game, authorized, position, rotation)
+        {
+
+        }
+    }
+}
