73645614ac31ac1babe3a440aa58ad37bb60d943
Alastair <c4dydl1@gmail.com>
2015-11-16 08:33:26 -0800

    Moved DRM client to new folder

--- prebuild.cmd
+++ prebuild.cmd
@@ -6,11 +6,14 @@ nuget restore %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln
 nuget restore %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln
 nuget restore %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln
 nuget restore %~dp0\MPTanks-MK5\MPTanks-MK5.sln
+nuget restore %~dp0\Infrastructure\ZSB.Drm.Client\ZSB.Drm.Client.sln
 
 %msbuildpath% %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Release
 %msbuildpath% %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Release
 %msbuildpath% %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Release
+%msbuildpath% %~dp0\Infrastructure\ZSB.Drm.Client\ZSB.Drm.Client.sln /t:Rebuild /p:Configuration=Release
 
 %msbuildpath% %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Debug
 %msbuildpath% %~dp0\Tools\MPTanks.MapMaker\MPTanks.MapMaker.sln /t:Rebuild /p:Configuration=Debug
 %msbuildpath% %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln /t:Rebuild /p:Configuration=Debug
+%msbuildpath% %~dp0\Infrastructure\ZSB.Drm.Client\ZSB.Drm.Client.sln /t:Rebuild /p:Configuration=Debug
