57758649fe1eb56c241099cfbff9fb590c4e18f2
Alastair <ala53@users.noreply.github.com>
2017-01-13 12:01:27 -0800

    Take 721,172: unbork build

--- asset_build.cmd
+++ asset_build.cmd
@@ -6,4 +6,4 @@
 "%2ekUiGen.exe" --input="%2assets\ui\xaml" --output="%2UI" --oa="%2assets\ui\imgs" --rm=MonoGame
 
 
-%~dp0Build\Tools\AssetCompileHelper\AssetCompileHelper.exe "" "%~dp0\Build Tools\MGCB\Windows\MGCB.exe" "%2\" "WindowsGL"
+%~dp0Build\Tools\AssetCompileHelper\AssetCompileHelper.exe "" "%~dp0\Build Tools\MGCB\Windows\MGCB.exe" %2 "WindowsGL"
