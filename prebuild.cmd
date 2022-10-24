032b22b8b65fa0eaa9af764c04a6b820a175656a
Alastair <ala53@users.noreply.github.com>
2022-10-23 19:12:12 -0700

    Bring the forbidden build pipeline into 2022

--- prebuild.cmd
+++ prebuild.cmd
@@ -1,13 +1,13 @@
-SET msbuildpath="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+SET msbuildpath="C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\bin\msbuild.exe"
 
 echo "blank" > %~dp0\MPTanks-MK5\CoreAssets\core-assets.mod
 
-nuget restore %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln
-nuget restore %~dp0\3rd_Party\Starbound.Input-3.18.2015\Starbound.Input.sln
-nuget restore %~dp0\3rd_Party\Lidgren.Network-8.31.2015\Lidgren.Network.sln
-nuget restore %~dp0\Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln
-nuget restore %~dp0\MPTanks-MK5\MPTanks-MK5.sln
-nuget restore %~dp0\Infrastructure\ZSB.Drm.Client\ZSB.Drm.Client.sln
+nuget restore %~dp0Tools\AssetCompileHelper\AssetCompileHelper.sln
+nuget restore %~dp03rd_Party\Starbound.Input-3.18.2015\Starbound.Input.sln
+nuget restore %~dp03rd_Party\Lidgren.Network-8.31.2015\Lidgren.Network.sln
+nuget restore %~dp0Tools\MPTanks.ModCompiler\MPTanks.ModCompiler.sln
+nuget restore %~dp0MPTanks-MK5\MPTanks-MK5.sln
+nuget restore %~dp0Infrastructure\ZSB.Drm.Client\ZSB.Drm.Client.sln
 
 %msbuildpath% %~dp0\Tools\AssetCompileHelper\AssetCompileHelper.sln /t:Rebuild /p:Configuration=Release
 %msbuildpath% %~dp0\3rd_Party\Starbound.Input-3.18.2015\Starbound.Input.sln /t:Rebuild /p:Configuration=Release
